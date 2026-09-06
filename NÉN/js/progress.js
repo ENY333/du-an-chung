db08e9a36deb9258ea507b464c4752be001668a0
ENY333 <matkhaucu0006@gmail.com>
2026-09-07 01:16:45 +0700

    Delete NÉN directory

--- NÉN/js/progress.js
+++
@@ -1,8 +0,0 @@
-function startCompressProgressPoll(taskId, cb) {
-    return setInterval(async () => {
-        try {
-            const res = await fetch('/api/get-progress?id=' + taskId);
-            if (res.ok) cb(await res.json());
-        } catch(e) {}
-    }, 500);
-}
